838cfa1989b36942e6bd45cd7ae86073455328bc
Cabbage74 <321027920@qq.com>
2026-06-11 03:09:16 +0800

    default dark

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "Cabbage74",
 	subtitle: "",
-	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 80, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: true, // Hide the theme color picker for visitors
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Cabbage74",
-	bio: "准备中",
+	bio: "",
 	links: [
 		// You will need to install the corresponding icon set if it's not already included
 		// `pnpm add @iconify-json/<icon-set-name>`
